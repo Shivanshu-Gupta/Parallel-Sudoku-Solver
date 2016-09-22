a6f796569b17c192958cc1ff019ef6ae2f8e28aa
Shivanshu-Gupta <shivanshugupta1995@gmail.com>
2016-09-22 21:20:27 +0530

    restructured project; added gitignore

--- make.sh
+++ make.sh
@@ -6,4 +6,6 @@ if [ $1 ]; then
 	minigridsize=$1
 fi
 
-g++ -DMINIGRIDSIZE=$minigridsize -fopenmp -O3 -Wall -g main.c sudoku.c -o sudoku
+cd src
+g++ -DMINIGRIDSIZE=$minigridsize -fopenmp -O3 -Wall -g main.c sudoku.c -o ../sudoku
+cd ..
